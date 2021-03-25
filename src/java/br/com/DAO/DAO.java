41adb5c90851d79c81ec914cc430d809c91a47b8
4b5al <diogo.a7x@live.com>
2021-03-25 01:23:01 -0300

    add DAO

---
+++ src/java/br/com/DAO/DAO.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class DAO {
+    Connection con; //ciração do objeto conexao
+    PreparedStatement pst; //criação preparação de ambiente
+    ResultSet rs; //criação do objeto que recebe resultados
+    
+    /**
+     *
+     * @throws SQLException
+     */
+    public void abrirBanco() throws SQLException{        
+        try{               
+            Class.forName("com.mysql.jdbc.Driver");
+            String localidade = "jdbc:mysql://localhost/SemestreDb";
+            String user = "root";
+            String senha = ""; 
+                con = (Connection) DriverManager.getConnection(localidade);
+                System.out.println("Conetado ao banco");
+        }catch(ClassNotFoundException ex){
+            System.out.println("Banco não encontrado");
+            Logger.getLogger(DAO.class.getName()).log(Level.SEVERE, null, ex);
+        }catch (SQLException e){
+            System.out.println(e);
+            throw new RuntimeException(e);
+        }
+    }  
+}
---
+++ src/java/br/com/DAO/TesteCon.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.DAO;
+
+import java.sql.SQLException;
+
+/**
+ *
+ * @author D1090
+ */
+public class TesteCon {
+    public static void main(String args[])throws SQLException{
+        DAO cx = new DAO();
+        
+        cx.abrirBanco();
+        System.out.println("deu certo!");
+    }
+    
+}
